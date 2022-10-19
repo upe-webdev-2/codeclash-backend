b571ce8a77b08e57ca6eedb89d4ca25a73fdaf59
David Ulloa <dulloa6310@gmail.com>
2022-10-19 14:34:27 -0400

    Added proper assert information

--- codeclash_backend/routes/execute.py
+++ codeclash_backend/routes/execute.py
@@ -18,44 +18,16 @@ def append_script(script : str, problem_info : dict) -> str:
     user_output_string += ")"
 
     script += user_output_string
-    script += '\n\t\tassert(user_return == test_cases.get("output"))'
+
+    script += '\n\t\tinput_details = ""'
+    script += '\n\t\tinput_details += '
+    script += r'f"\n{input}" for input in case["inputs"]'
+
+    script += '\n\t\tassert user_return == test_cases.get("output")'
+    script += r',f"Your Input: {input_details}\nYour Output: {user_return}\nExpected Output: {test_cases.get("output")}"'
 
     return script
 
-@execute.route('/')
+@execute.route('/', methods = ["POST"])
 def index():
-    problem_example = {
-        "id": 1,
-        "title": "Two Sum",
-        "difficulty": "Easy",
-        "objectives": [
-            "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.",
-            "You may assume that each input would have exactly one solution, and you may not use the same element twice.",
-            "You can return the answer in any order."
-        ],
-        "examples": [
-            {
-            "input": "nums = [2,7,11,15], target = 9",
-            "output": "[0,1]",
-            "explanation": "Because nums[0] + nums[1] == 9, we return [0, 1]."
-            },
-            {
-            "input": "nums = [3,2,4], target = 6",
-            "output": "[1,2]"
-            },
-            {
-            "input": "nums = [3,3], target = 6",
-            "output": "[0,1]"
-            }
-        ],
-        "starterCode": "def twoSum(nums: List[int], target: int) -> List[int]:\n\t# Code here...\n\tpass",
-        "testCases": [{"inputs": [2], "output": 4}, {"inputs": [4], "output": 16}],
-        "functionName": "solve"
-    }
-
-    string = append_script("def solve(num):\n\treturn num ** 2", problem_example)
-    print(string)
-
-    return {
-        "script": string,
-    }
+    return {}
