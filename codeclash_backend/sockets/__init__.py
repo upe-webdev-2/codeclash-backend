9216bc60a56dc5fa2117229dbc55ca4fc4986f98
David Ulloa <dulloa6310@gmail.com>
2022-10-30 20:03:53 -0400

    Added rooms

--- codeclash_backend/sockets/__init__.py
+++ codeclash_backend/sockets/__init__.py
@@ -1,11 +1,22 @@
 from codeclash_backend import socketio
+from flask_socketio import send, join_room, close_room
+
+rooms = {}
 
 @socketio.on('connect')
 def connect(auth):
-    print(auth)
-    emit('test_connection', {'data': 'connected'})
+    if len(rooms) == 0:
+        rooms[auth.roomName] = 1
+        join_room(auth.roomName)
+    else:
+        for room in rooms.keys():
+            if rooms[room] <= 1:
+                rooms[room] += 1
+                join_room(auth.roomName)
+    send({'message': f'User connected, sent to room {auth.roomName}'})
 
 @socketio.on('disconnect')
 def disconnect(auth):
-    print(auth)
-    emit('test_disconnect', {'data' : 'disconnected'})
+    rooms.remove(auth.roomName)
+    close_room(auth.roomName)
+    send({'message' : f'User disconnected, destroyed room {auth.roomName}'})
