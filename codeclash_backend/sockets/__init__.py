193088af57b048a3e44308d8dd2e27c9b0e08e5d
David Ulloa <dulloa6310@gmail.com>
2022-11-02 14:50:28 -0400

    Finished Basic Sockets

--- codeclash_backend/sockets/__init__.py
+++ codeclash_backend/sockets/__init__.py
@@ -6,8 +6,7 @@ from flask import request
 waiting_room = []
 rooms = {}
 
-def find_room(username = None: str, user_id = None: str, rooms : dict) -> dict:
-
+def find_room(rooms = dict, username = None, user_id = None) -> dict:
     if username is None and user_id is None:
         return {}
 
@@ -46,7 +45,7 @@ def join_game(data):
         emit("finishedWaitingRoom", {"roomName" : room_name, "opponentName" : second_player_name}, namespace = "/play")
         emit("finishedWaitingRoom", {"roomName" : room_name, "opponentName" : first_player_name}, namespace = "/play", to = second_player_id)
         
-        rooms[room_name] = f"{first_player_name} {second_player_name}"
+        rooms[room_name] = [first_player_name, second_player_name]
     else:
         waiting_room.append((first_player_id, first_player_name))
 
@@ -62,7 +61,7 @@ def player_leave(data):
     lost_player_id = request.sid
     lost_player_name = data.get("username")
 
-    room_info = find_room(lost_player_name, lost_player_id, rooms)
+    room_info = find_room(rooms, lost_player_name, lost_player_id)
 
     if len(room_info) == 0:
         # User not in a room. Maybe return an error?
@@ -75,5 +74,26 @@ def player_leave(data):
 
     del rooms[room_name]
 
-    emit("finishedGame", {"wonPlayer" : won_player_name, "lostPlayer" : lost_player_name}, namespace = "/play", to = room)
+    emit("finishedGame", {"wonPlayer" : won_player_name, "lostPlayer" : lost_player_name}, namespace = "/play", to = room_name)
+    close_room(room_name)
+
+@socketio.on("playerWin", namespace = "/play")
+def player_leave(data):
+    won_player_id = request.sid
+    won_player_name = data.get("username")
+
+    room_info = find_room(rooms, won_player_name, won_player_name)
+
+    if len(room_info) == 0:
+        # User not in a room. Maybe return an error?
+        return
+
+    room_name = room_info.get("room_name")
+    room_players = room_info.get("players")
+
+    lost_player_name = room_players[1] if room_players[0] == won_player_name else room_players[0]
+
+    del rooms[room_name]
+
+    emit("finishedGame", {"wonPlayer" : won_player_name, "lostPlayer" : lost_player_name}, namespace = "/play", to = room_name)
     close_room(room_name)
