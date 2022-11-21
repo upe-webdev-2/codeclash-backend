486e7f56fe513ef432c28da8433567944b9d4b3a
Gino Rey <gino.rey@Outlook.com>
2022-11-20 19:56:30 -0500

    Add files via upload

---
+++ redeploy.sh
@@ -0,0 +1,10 @@
+`#!/bin/bash
+
+# cd into working dir
+cd ~/codeclash/codeclash_backend
+# reset git branch
+git fetch && git reset origin/main -- hard
+# spin down containers
+docker compose -f docker-compose.prod.yml down
+# spin containers back up
+docker compose -f docker-compose.prod.yml up -d --build
