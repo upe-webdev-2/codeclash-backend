fe999cf31255f03f576d31c90b603eca49776713
David Ulloa <dulloa6310@gmail.com>
2022-11-27 16:11:51 -0500

    Added default problem info on rand_problem

--- codeclash_backend/routes/problem.py
+++ codeclash_backend/routes/problem.py
@@ -43,7 +43,34 @@ def rand_problem() -> Union[dict, None]:
     problem_count = prisma.problem.count()
     
     if problem_count == 0:
-        return None
+        return {
+        "id": 0,
+        "title": "Two Sum",
+        "difficulty": "Easy",
+        "objectives": [
+            "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.",
+            "You may assume that each input would have exactly one solution, and you may not use the same element twice.",
+            "You can return the answer in any order."
+        ],
+        "examples": [
+            {
+            "input": "nums = [2,7,11,15], target = 9",
+            "output": "[0,1]",
+            "explanation": "Because nums[0] + nums[1] == 9, we return [0, 1]."
+            },
+            {
+            "input": "nums = [3,2,4], target = 6",
+            "output": "[1,2]"
+            },
+            {
+            "input": "nums = [3,3], target = 6",
+            "output": "[0,1]"
+            }
+        ],
+        "starterCode": 'def twoSum(nums: List[int], target: int) -> List[int]:\n\t# Code here...\n\tpass',
+        "testCases": [{"inputs": [[2,7,11,15], 9], "output": [0,1]}, {"inputs": [[1,1,2,3,4,6,7], 8], "output": [2,5]}],
+        "functionName": "twoSum"
+        }
     
     random_id = random.randint(0, problem_count - 1)
 
