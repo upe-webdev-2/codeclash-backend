1598e085202412154b7863803785b9ecd3c4a9a5
Gino Rey <gino.rey@outlook.com>
2022-12-01 18:49:29 -0500

    docker and redeploy changes

--- redeploy.sh
+++ redeploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# reset git branch
-git fetch && git reset origin/main -- hard
+# reset backend git branch
+cd ~/codeclash/codeclash-backend
+git fetch && git reset origin/main --hard
+
+# reset frontend git branch
+cd ~/codeclash/codeclash-frontend
+git fetch && git reset origin/master --hard
+
 # spin down containers
+cd ~/codeclash
 docker compose -f docker-compose.prod.yml down
 # spin containers back up
 docker compose -f docker-compose.prod.yml up -d --build
