288e83317c4f62c6002a310896e6075537f6116d
David Ulloa <dulloa6310@gmail.com>
2022-12-01 14:12:01 -0500

    Added disconnect event

--- codeclash_backend/sockets/player.py
+++ codeclash_backend/sockets/player.py
@@ -58,6 +58,40 @@ def player_leave(data):
     delete_room(room_name)
     close_room(room_name, namespace = "/play")
 
+@socketio.on("disconnect", namespace = namespace)
+def disconnect(data):
+    disconnect_player_id = request.sid
+
+    if in_waiting_room(player_id = disconnect_player_id):
+        remove_from_waiting_room(disconnect_player_id)
+        return
+
+    room = find_room(user_id = disconnect_player_id)
+
+    if len(room) == 0:
+        return
+
+    room_name = room.get("roomName")
+    room_players = room.get("players")
+    room_player_ids = room_name.split(" ")
+
+    if room_player_ids[0] == disconnect_player_id:
+        lost_player_name = room_players[0]
+        won_player_name = room_players[1]
+    else:
+        lost_player_name = room_players[1]
+        won_player_name = room_players[0]
+
+    won_player_info = get_user(won_player_name)
+    lost_player_info = get_user(lost_player_name)
+
+    emit("finishedGame", {"wonPlayer" : won_player_name, "lostPlayer" : lost_player_name, "wonPlayerInfo" : won_player_info, "lostPlayerInfo" : lost_player_info}, namespace = "/play", to = room_name.split(" ")[0])
+    emit("finishedGame", {"wonPlayer" : won_player_name, "lostPlayer" : lost_player_name, "wonPlayerInfo" : won_player_info, "lostPlayerInfo" : lost_player_info}, namespace = "/play", to = room_name.split(" ")[1])
+
+    delete_room(room_name)
+    close_room(room_name, namespace = "/play")
+
+
 @socketio.on("playerWin", namespace = namespace)
 def player_win(data):
     won_player_name = data.get("username")
